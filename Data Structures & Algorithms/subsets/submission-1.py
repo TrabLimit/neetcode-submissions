8a424cf3e20bb0692cf34776c3135d2bc895ffed
Bartholomew Lim <134988675+TrabLimit@users.noreply.github.com>
2026-08-18 16:22:54 -0700

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.py
@@ -0,0 +1,75 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+
+        # does num contain negative or 0? -> yes (range from -10 to 10)
+        # does is num sorted? -> no
+        # is num empty? -> no
+        # nums contain unique integers
+        
+
+        # test case:
+        # nums = [1]
+        # output = [[], [1]]
+
+        # nums = [0, 1, 2]
+        # output = [[], [0], [0, 1], [0, 1, 2], [1], [1, 2], [2]]
+
+
+        # brute force:
+        # loop over every element and make a subset out of them
+        # dangerous -> potentially double calculate
+        # runtime: O(n^2)
+
+        # optimal:
+        # for every element, you have 2 choices: add or not add
+        # so runtime is O(2^n)
+
+        # nums = [0, 1, 2]
+
+        # make curr = []
+        # make result = []
+        
+        # i = 0 
+        # add 0 to curr -> curr = [0]
+        # i = 1
+        # add 1 to curr -> curr [0, 1]
+        # i = 2 
+        # add 2 to curr -> curr [0, 1, 2]
+
+        # i = 3 
+        # base case hit 
+        # add curr [0, 1, 2] to result
+        # result = [[0, 1, 2]]
+
+        result = []
+
+        curr = []
+
+        def dfs(i: int):
+            if i == len(nums):
+                result.append(curr.copy()) # since we pass by reference, so we pass the copy so any modification to curr is not affecting the original
+                return
+            
+            # 1. skip the element
+            
+            dfs(i+1)
+
+            # 2. add the element
+           
+            curr.append(nums[i])
+            dfs(i+1)
+            # make sure you pop the previously added nums[i] after done adding (to reset)
+            curr.pop()
+        
+        dfs(0)
+
+        return result
+    
+
+
+ 
+
+
+
+
+        
